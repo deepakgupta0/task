691df3186b8db32bc2708db99242615087fe056e
deepakgupta0 <deepakgupta08112000@gmail.com>
2024-06-05 16:42:10 +0530

    fixed producer code

--- poll-widget/src/index.js
+++ poll-widget/src/index.js
@@ -12,3 +12,42 @@ export function initialize(selector, data) {
   const root = ReactDOM.createRoot(document.getElementById(`${selector}`));
   root.render(<App {...data} />);
 }
+
+const pollQuestionsSet1 = [
+  {
+    id: 1,
+    question: "What is your favorite programming language?",
+    options: ["JavaScript", "Python", "Java", "C++", "Ruby"],
+  },
+  {
+    id: 2,
+    question: "What is your favorite movie genre?",
+    options: ["Action", "Comedy", "Drama", "Science Fiction", "Horror"],
+  },
+  {
+    id: 3,
+    question: "Which social media platform do you use the most?",
+    options: ["Facebook", "Instagram", "Twitter", "LinkedIn", "Snapchat"],
+  },
+  {
+    id: 4,
+    question: "What is your favorite season?",
+    options: [
+      "Spring",
+      "Summer",
+      "Autumn",
+      "Winter",
+      "I don't have a favorite",
+    ],
+  },
+  {
+    id: 5,
+    question: "How do you prefer to travel?",
+    options: ["Car", "Train", "Airplane", "Bicycle", "Walking"],
+  },
+];
+
+initialize("root", {
+  questions: pollQuestionsSet1,
+  pollWidgetId: "test-poll-1",
+});
